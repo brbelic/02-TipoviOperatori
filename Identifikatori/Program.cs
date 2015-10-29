770233a5e4fe0c9fc6e90287432f50de3d303e27
Branimir <br.belic@gmail.com>
2015-10-29 21:12:44 +0100

    Identifikatorske varijable i njihovi unosi

--- Identifikatori/Program.cs
+++ Identifikatori/Program.cs
@@ -9,6 +9,17 @@ namespace CSharp.TipoviOperatori
     {
         static void Main(string[] args)
         {
+            int @string = 5;  //stavili smo @ jer je string kljucna rijec i ne moze se koristiti kao identifikator
+
+            string get = "Pero";
+
+            double Main = 3.14;
+
+            long četrdeset\u0161est = 46;
+
+            Console.WriteLine(četrdesetšest);
+
+
         }
     }
 }
